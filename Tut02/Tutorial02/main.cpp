47a87bf5013a886239eb2af1ed10a9bb237244ee
adautev <andrei@dautev.eu>
2019-07-28 13:41:58 +0300

    Basic two tutorials initialized

---
+++ Tut02/Tutorial02/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+//TODO: https://github.com/JoeyDeVries/LearnOpenGL/blob/master/src/1.getting_started/2.1.hello_triangle/hello_triangle.cpp
+//TODO: + http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
