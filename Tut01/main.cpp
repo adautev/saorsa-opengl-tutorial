190f3a68754d97982d6455fd20d0d8fb02540645
adautev <andrei@dautev.eu>
2019-07-28 13:43:48 +0300

    Added code for window tut

---
+++ Tut01/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
